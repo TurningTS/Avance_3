f85c61b7e098315cf2d523b6402358157f001416
TurningTS <89107942+TurningTS@users.noreply.github.com>
2021-10-25 22:00:08 -0500

    Add files via upload

---
+++ registro.php
@@ -0,0 +1,34 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>DoctorReserva</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <link rel="stylesheet" href="bootstrap/bootstrap.min.css">
+        <link rel="stylesheet" href="bootstrap/mdb.min.css">
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+        <header>
+            <nav class="navbar fixed-top navbar-expand-md scrolling-navbar justify-content-end">
+                <a class="nav-link" href="index.html"><strong>Inicio</strong></a>
+                <a href="contacto.html" class="nav-link">Contacto</a>
+                <a href="Registro.html" class="nav-link">Registro</a>
+                <a href="acerca.html" class="nav-link">Acerca de</a>
+            </nav>
+            <div class="view registro">
+                <div class="flex-center">
+                    <a href="#" class="btn btn-primary"><h4>Registro exitoso</h4></a>
+                </div>
+            </div>
+        </header>
+        <footer class="w-100 py-4 flex-shrink-0">
+        </footer>
+        <script src="js/scroll.js"></script>
+    </body>
+</html>
